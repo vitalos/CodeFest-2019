340bbabf179eefe1dfff1bfdb7a14a0e9ca0c7d4
vitalos <ljv27@drexel.edu>
2019-05-05 03:44:11 +0000

    Revert "Merge pull request #1 from vitalos/glitch"
    This reverts commit 731a27499816a2b8f31e5051a8f6e88b02134154, reversing
    changes made to 139dff1a23f4baa9716cfde5df34c3ecf11116eb.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+$(document).ready(function () {
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
